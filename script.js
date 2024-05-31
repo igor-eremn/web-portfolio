7f8543de050c2148408fd16cdb946d2ee115ec8c
Igor Eremin <77459655+success-412@users.noreply.github.com>
2024-05-30 20:01:23 -0700

    added back about and skills sections with proper script

--- script.js
+++ script.js
@@ -9,19 +9,23 @@ function animateBars() {
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('nav a');
     const sections = document.querySelectorAll('.section-container');
-  
+
     links.forEach(link => {
-      link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const target = event.target.getAttribute('data-section');
-        sections.forEach(section => {
-          if (section.id === target) {
-            section.style.display = 'flex';
-          } else {
-            section.style.display = 'none';
-          }
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const target = event.target.getAttribute('data-section');
+            sections.forEach(section => {
+                if (section.id === target) {
+                    section.style.display = 'flex';
+                    
+                    // Call animateBars function if the skills section is displayed
+                    if (target === 'skills') {
+                        animateBars();
+                    }
+                } else {
+                    section.style.display = 'none';
+                }
+            });
         });
-      });
     });
-  });
-  
+});
